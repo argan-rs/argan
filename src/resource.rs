8f9137288a18ef4bb0a79c57dddf20d581a3d144
Shohruh Adham <shohruh.adham@gmail.com>
2023-05-30 16:14:19 +0500

    feat: add synchronous implementations of request_receiver and request_passer

--- src/resource.rs
+++ src/resource.rs
@@ -1,3 +1,5 @@
+use hyper::StatusCode;
+
 use super::utils::*;
 
 // --------------------------------------------------
@@ -14,7 +16,117 @@ pub struct Resource {
 	request_handler: Option<BoxedService>,
 
 	// TODO: configs, state, redirect, parent
-
-	is_subtree_handler: bool
+	is_subtree_handler: bool,
 }
 
+// -------------------------
+
+impl Resource {
+	#[inline]
+	pub fn is_subtree_handler(&self) -> bool {
+		self.is_subtree_handler
+	}
+
+	#[inline]
+	pub fn can_handle_request(&self) -> bool {
+		self.request_handler.is_some()
+	}
+
+	fn request_receiver(&self, mut req: Request) -> Result<Response, BoxedError> {
+		let rs = req.extensions_mut().get_mut::<RoutingState>().unwrap();
+
+		if rs.path_segments.has_remaining_segments() {
+			if self.is_subtree_handler() {
+				rs.subtree_handler_exists = true;
+			}
+
+			req = match self.request_passer(req) {
+				Ok(mut res) => {
+					if res.status() != StatusCode::NOT_FOUND
+						|| !self.is_subtree_handler()
+						|| !self.can_handle_request()
+					{
+						return Ok(res);
+					}
+
+					let Some(unused_request) = res.extensions_mut().remove::<UnusedRequest>() else {
+						// Some middleware or handler set by the user has used the request and returned
+						// NOT_FOUND.
+						return Ok(res);
+					};
+
+					unused_request.into()
+				}
+				err => return err,
+			}
+		}
+
+		return self.request_handler(req);
+	}
+
+	fn request_passer(&self, mut req: Request) -> Result<Response, BoxedError> {
+		let rs = req.extensions_mut().get_mut::<RoutingState>().unwrap();
+
+		// request_passer wouldn't be called if there wasn't any segment left. So, we can unwrap safely.
+		let next_path_segment = rs.path_segments.next().unwrap();
+
+		let some_next_resource = 'some_next_resource: {
+			if let Some(next_resource) = self.static_resources.as_ref().and_then(|static_resources| {
+				match static_resources
+					.binary_search_by(|resource| resource.name.cmp(next_path_segment.as_str()))
+				{
+					Ok(i) => Some(&static_resources[i]),
+					Err(_) => None,
+				}
+			}) {
+				break 'some_next_resource Some(next_resource);
+			}
+
+			None
+		};
+
+		if let Some(next_resource) = some_next_resource {
+			match next_resource.request_receiver(req) {
+				Ok(mut res) => {
+					if res.status() != StatusCode::NOT_FOUND
+						|| !self.is_subtree_handler()
+						|| !self.can_handle_request()
+					{
+						return Ok(res);
+					}
+
+					let Some(unused_request) = res.extensions_mut().get_mut::<UnusedRequest>() else {
+						// Some middleware or a service set by user has used the request and returned NOT_FOUND.
+						return Ok(res);
+					};
+
+					// Unwrap safety: returned request is guaranteed to have a RoutingState.
+					let rs = unused_request
+						.as_mut()
+						.extensions_mut()
+						.get_mut::<RoutingState>()
+						.unwrap();
+					rs.path_segments.revert_to_segment(next_path_segment);
+
+					return Ok(res);
+				}
+				err => return err,
+			}
+		}
+
+		let mut response = Response::default();
+		*response.status_mut() = StatusCode::NOT_FOUND;
+
+		if rs.subtree_handler_exists {
+			rs.path_segments.revert_to_segment(next_path_segment);
+			response.extensions_mut().insert(req);
+		}
+
+		Ok(response)
+	}
+
+	#[inline]
+	fn request_handler(&self, req: Request) -> Result<Response, BoxedError> {
+		todo!()
+	}
+}
--- src/utils.rs
+++ src/utils.rs
@@ -33,7 +33,7 @@ impl From<Request> for UnusedRequest {
 
 impl From<UnusedRequest> for Request {
 	fn from(value: UnusedRequest) -> Self {
-		value.0	
+		value.0
 	}
 }
 
@@ -61,14 +61,17 @@ impl<'req> PathSegments<'req> {
 	#[inline]
 	pub(crate) fn new(path: &'req str) -> PathSegments<'_> {
 		// Path contains at least a slash or begins with one.
-		Self{path, remaining_segments_index: 0}
+		Self {
+			path,
+			remaining_segments_index: 0,
+		}
 	}
 
 	#[inline]
 	pub(crate) fn has_remaining_segments(&self) -> bool {
 		!self.path.is_empty()
 	}
-	
+
 	#[inline]
 	pub(crate) fn revert_to_segment(&mut self, segment: PathSegment) {
 		self.remaining_segments_index = segment.index;
@@ -81,7 +84,7 @@ impl<'req> Iterator for PathSegments<'req> {
 	#[inline]
 	fn next(&mut self) -> Option<Self::Item> {
 		if self.remaining_segments_index == self.path.len() {
-			return None
+			return None;
 		}
 
 		let next_segment_start_index = self.remaining_segments_index;
@@ -90,13 +93,19 @@ impl<'req> Iterator for PathSegments<'req> {
 		let Some(next_segment_end_index) = remaining_segments.find('/') else {
 			self.remaining_segments_index = self.path.len();
 
-			return Some(PathSegment{value: remaining_segments, index: next_segment_start_index})
+			return Some(PathSegment {
+				value: remaining_segments,
+				index: next_segment_start_index,
+			});
 		};
 
 		self.remaining_segments_index += next_segment_end_index;
 		let next_segment = &remaining_segments[..next_segment_end_index];
 
-		Some(PathSegment{value: next_segment, index: next_segment_start_index})
+		Some(PathSegment {
+			value: next_segment,
+			index: next_segment_start_index,
+		})
 	}
 }
 
@@ -110,4 +119,3 @@ impl<'req> PathSegment<'req> {
 		self.value
 	}
 }
-
