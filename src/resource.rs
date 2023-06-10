e7eb65fb686025fa5fcf73e6301afd2ecd4671c1
Shohruh Adham <19423545+shohruhadham@users.noreply.github.com>
2023-06-10 10:49:53 +0500

    feat: rewrite request_receiver and request_passer as async functions

--- src/resource.rs
+++ src/resource.rs
@@ -1,4 +1,13 @@
+use std::{future::Future, pin::Pin};
+
 use hyper::StatusCode;
+
+use crate::{
+	request::Request,
+	response::Response,
+	routing::{RoutingState, UnusedRequest},
+	service::BoxedService,
+};
 
 use super::utils::*;
 
@@ -31,102 +40,100 @@ impl Resource {
 	pub fn can_handle_request(&self) -> bool {
 		self.request_handler.is_some()
 	}
+}
 
-	fn request_receiver(&self, mut req: Request) -> Result<Response, BoxedError> {
-		let rs = req.extensions_mut().get_mut::<RoutingState>().unwrap();
+fn request_receiver(
+	mut request: Request,
+) -> Pin<Box<dyn Future<Output = Result<Response, BoxedError>>>> {
+	Box::pin(async move {
+		let mut rs = request.extensions_mut().get_mut::<RoutingState>().unwrap();
+		let cr = rs.current_resource.unwrap();
 
 		if rs.path_segments.has_remaining_segments() {
-			if self.is_subtree_handler() {
+			if cr.is_subtree_handler() {
 				rs.subtree_handler_exists = true;
 			}
 
-			req = match self.request_passer(req) {
-				Ok(mut res) => {
-					if res.status() != StatusCode::NOT_FOUND
-						|| !self.is_subtree_handler()
-						|| !self.can_handle_request()
-					{
-						return Ok(res);
-					}
-
-					let Some(unused_request) = res.extensions_mut().remove::<UnusedRequest>() else {
-						// Some middleware or handler set by the user has used the request and returned
-						// NOT_FOUND.
-						return Ok(res);
+			request = match cr.request_passer.as_ref() {
+				Some(request_passer) => {
+					let result = request_passer.clone_boxed().call(request).await;
+					let Ok(mut response) = result else {
+						return result;
 					};
 
-					unused_request.into()
-				}
-				err => return err,
-			}
-		}
-
-		return self.request_handler(req);
-	}
-
-	fn request_passer(&self, mut req: Request) -> Result<Response, BoxedError> {
-		let rs = req.extensions_mut().get_mut::<RoutingState>().unwrap();
-
-		// request_passer wouldn't be called if there wasn't any segment left. So, we can unwrap safely.
-		let next_path_segment = rs.path_segments.next().unwrap();
-
-		let some_next_resource = 'some_next_resource: {
-			if let Some(next_resource) = self.static_resources.as_ref().and_then(|static_resources| {
-				match static_resources
-					.binary_search_by(|resource| resource.name.cmp(next_path_segment.as_str()))
-				{
-					Ok(i) => Some(&static_resources[i]),
-					Err(_) => None,
-				}
-			}) {
-				break 'some_next_resource Some(next_resource);
-			}
-
-			None
-		};
-
-		if let Some(next_resource) = some_next_resource {
-			match next_resource.request_receiver(req) {
-				Ok(mut res) => {
-					if res.status() != StatusCode::NOT_FOUND
-						|| !self.is_subtree_handler()
-						|| !self.can_handle_request()
+					if response.status() != StatusCode::NOT_FOUND
+						|| !cr.is_subtree_handler()
+						|| !cr.can_handle_request()
 					{
-						return Ok(res);
+						return Ok(response);
 					}
 
-					let Some(unused_request) = res.extensions_mut().get_mut::<UnusedRequest>() else {
-						// Some middleware or a service set by user has used the request and returned NOT_FOUND.
-						return Ok(res);
+					let Some(unused_request) = response.extensions_mut().remove::<UnusedRequest>() else {
+						return Ok(response);
 					};
 
-					// Unwrap safety: returned request is guaranteed to have a RoutingState.
-					let rs = unused_request
-						.as_mut()
-						.extensions_mut()
-						.get_mut::<RoutingState>()
-						.unwrap();
-					rs.path_segments.revert_to_segment(next_path_segment);
-
-					return Ok(res);
+					unused_request.into_request()
 				}
-				err => return err,
+				None => {
+					let result = request_passer(request).await;
+					let Ok(mut response) = result else {
+						return result;
+					};
+
+					if response.status() != StatusCode::NOT_FOUND
+						|| !cr.is_subtree_handler()
+						|| !cr.can_handle_request()
+					{
+						return Ok(response);
+					}
+
+					let Some(unused_request) = response.extensions_mut().remove::<UnusedRequest>() else {
+						return Ok(response);
+					};
+
+					unused_request.into_request()
+				}
+			};
+		}
+
+		request_handler(request).await
+	})
+}
+
+async fn request_passer(mut req: Request) -> Result<Response, BoxedError> {
+	let rs = req.extensions_mut().get_mut::<RoutingState>().unwrap();
+	let cr = rs.current_resource.unwrap();
+
+	let next_path_segment = rs.path_segments.next().unwrap();
+
+	let some_next_resource = 'some_next_resource: {
+		if let Some(next_resource) = cr.static_resources.as_ref().and_then(|static_resources| {
+			match static_resources
+				.binary_search_by(|resource| resource.name.cmp(next_path_segment.as_str()))
+			{
+				Ok(i) => Some(&static_resources[i]),
+				Err(_) => None,
 			}
+		}) {
+			break 'some_next_resource Some(next_resource);
 		}
 
-		let mut response = Response::default();
-		*response.status_mut() = StatusCode::NOT_FOUND;
+		None
+	};
 
-		if rs.subtree_handler_exists {
-			rs.path_segments.revert_to_segment(next_path_segment);
-			response.extensions_mut().insert(req);
-		}
-
-		Ok(response)
+	if let Some(next_resource) = some_next_resource {
+		rs.current_resource.replace(next_resource);
+		return request_receiver(req).await;
 	}
 
-	#[inline]
-	fn request_handler(&self, req: Request) -> Result<Response, BoxedError> {
-		todo!()
-	}
+	let mut response = Response::default();
+	*response.status_mut() = StatusCode::NOT_FOUND;
+
+	response.extensions_mut().insert(UnusedRequest::from(req));
+
+	Ok(response)
+}
+
+async fn request_handler(_req: Request) -> Result<Response, BoxedError> {
+	todo!()
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,6 @@
-
 // --------------------------------------------------
 
-pub type Request = hyper::Request<hyper::Body>;
-pub type Response = hyper::Response<hyper::Body>;
-pub type BoxedError = tower::BoxError;
-pub type BoxPinnedFuture = futures::future::BoxFuture<'static, Result<Response, BoxedError>>;
-pub type BoxedService = Box<
-	dyn tower::Service<Request, Response = Response, Error = BoxedError, Future = BoxPinnedFuture>
-		+ Send
-		+ Sync,
->;
+pub type BoxedError = Box<dyn std::error::Error + Send + Sync>;
 
 // --------------------------------------------------
 
