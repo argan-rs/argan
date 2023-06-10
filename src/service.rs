d37883ee5230064b322ea087822c3faec59db513
Shohruh Adham <19423545+shohruhadham@users.noreply.github.com>
2023-06-10 10:43:17 +0500

    feat: add ServiceFn

--- src/service.rs
+++ src/service.rs
@@ -1,9 +1,6 @@
 use std::future::Future;
 
-pub use tower::{
-	util::{service_fn, ServiceFn},
-	Service,
-};
+use hyper::service::Service;
 
 use super::utils::*;
 
@@ -18,22 +15,53 @@ where
 
 // -------------------------
 
-impl<Func, F> IntoService<ServiceFn<Func>, Request> for Func
+impl<S, Req> IntoService<S, Req> for S
 where
-	Func: FnMut(Request) -> F,
-	F: Future<Output = Result<Response, BoxedError>>,
+	S: Service<Req>,
 {
-	fn into_service(self) -> ServiceFn<Func> {
-		service_fn(self)
+	fn into_service(self) -> S {
+		self
 	}
 }
 
-impl<T, Req> IntoService<T, Req> for T
+impl<Func, Fut, Req, Res, Err> IntoService<ServiceFn<Func>, Req> for Func
 where
-	T: Service<Req>,
+	Func: FnMut(Req) -> Fut,
+	Fut: Future<Output = Result<Res, Err>>,
+	Err: Into<BoxedError>,
 {
-	fn into_service(self) -> T {
-		self
+	#[inline]
+	fn into_service(self) -> ServiceFn<Func> {
+		ServiceFn { func: self }
+	}
+}
+
+// --------------------------------------------------
+
+#[derive(Clone)]
+pub struct ServiceFn<Func> {
+	func: Func,
+}
+
+impl<Func, Fut, Req, Res, Err> Service<Req> for ServiceFn<Func>
+where
+	Func: FnMut(Req) -> Fut,
+	Fut: Future<Output = Result<Res, Err>>,
+{
+	type Response = Res;
+	type Error = Err;
+	type Future = Fut;
+
+	#[inline]
+	fn call(&self, req: Req) -> Self::Future {
+		(self.func)(req)
+	}
+}
+
+impl<Func> std::convert::From<Func> for ServiceFn<Func> {
+	#[inline]
+	fn from(value: Func) -> Self {
+		Self { func: value }
 	}
 }
 
@@ -41,7 +69,13 @@ where
 
 #[cfg(test)]
 mod test {
+	use std::pin::Pin;
+
+	use crate::{request::Request, response::Response};
+
 	use super::*;
+
+	// -------------------------
 
 	fn is_service<T, S, Req>(_: T)
 	where
@@ -50,22 +84,31 @@ mod test {
 	{
 	}
 
-	async fn handler(_: Request) -> Result<Response, BoxedError> {
-		unimplemented!()
+	// -------------------------
+
+	async fn handler(_: Request<()>) -> Result<Response<()>, BoxedError> {
+		todo!()
 	}
 
-	async fn handler_mut(mut _unused: Request) -> Result<Response, BoxedError> {
-		unimplemented!()
+	async fn handler_mut(mut _unused: Request<()>) -> Result<Response<()>, BoxedError> {
+		todo!()
 	}
 
-	fn handler_returning_future(_: Request) -> BoxPinnedFuture {
-		unimplemented!()
+	fn handler_returning_future(
+		_: Request<()>,
+	) -> Pin<Box<dyn Future<Output = Result<Response<()>, BoxedError>>>> {
+		todo!()
 	}
+
+	// -------------------------
 
 	#[test]
 	fn test() {
 		is_service(handler);
 		is_service(handler_mut);
-		is_service(handler_returning_future)
+		is_service(handler_returning_future);
+
+		let boxed_service = Box::new(handler);
+		is_service(boxed_service);
 	}
 }
