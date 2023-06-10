b37960ec9dd743299bc97d6edd6f594cb1b5df6e
Shohruh Adham <shohruh.adham@gmail.com>
2023-06-10 10:35:15 +0500

    feat: add Request, FromRequest, Response, IntoResponse, hyper body types

---
+++ src/body.rs
@@ -0,0 +1,5 @@
+pub use hyper::body::{Body, Bytes, Incoming};
+
+pub(crate) use http_body_util::combinators::BoxBody as BoxedBody;
+
+// --------------------------------------------------
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![allow(dead_code)]
 
+mod body;
+mod request;
 mod resource;
+mod response;
 mod routing;
 mod service;
 mod utils;
