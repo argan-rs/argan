ead13b9ba106150735269347646bc1c2564bf546
Shohruh Adham <19423545+shohruhadham@users.noreply.github.com>
2023-06-11 11:07:48 +0500

    feat: check the resource's request_receiver in the request_passer

--- src/resource.rs
+++ src/resource.rs
@@ -42,111 +42,100 @@ impl Resource {
 	}
 }
 
-async fn request_receiver(mut request: Request) -> Result<Response, BoxedError> {
-    let mut rs = request.extensions_mut().get_mut::<RoutingState>().unwrap();
-    let cr = rs.current_resource.unwrap();
+fn request_receiver(
+	mut request: Request,
+) -> Pin<Box<dyn Future<Output = Result<Response, BoxedError>>>> {
+	Box::pin(async move {
+		let mut rs = request.extensions_mut().get_mut::<RoutingState>().unwrap();
+		let cr = rs.current_resource.unwrap();
 
-    if rs.path_segments.has_remaining_segments() {
-        if cr.is_subtree_handler() {
-            rs.subtree_handler_exists = true;
-        }
+		if rs.path_segments.has_remaining_segments() {
+			if cr.is_subtree_handler() {
+				rs.subtree_handler_exists = true;
+			}
 
-        request = match cr.request_passer.as_ref() {
-            Some(request_passer) => {
-                let result = request_passer.clone_boxed().call(request).await;
-                let Ok(mut response) = result else {
-                    return result;
-                };
+			let result = match cr.request_passer.as_ref() {
+				Some(request_passer) => request_passer.clone_boxed().call(request).await,
+				None => request_passer(request).await,
+			};
 
-                if response.status() != StatusCode::NOT_FOUND
-                    || !cr.is_subtree_handler()
-                    || !cr.can_handle_request()
-                {
-                    return Ok(response);
-                }
+			let Ok(mut response) = result else {
+				return result;
+			};
 
-                let Some(unused_request) = response.extensions_mut().remove::<UnusedRequest>()
-                else {
-                    return Ok(response);
-                };
+			if response.status() != StatusCode::NOT_FOUND
+				|| !cr.is_subtree_handler()
+				|| !cr.can_handle_request()
+			{
+				return Ok(response);
+			}
 
-                unused_request.into_request()
-            }
-            None => {
-                let result = request_passer(request).await;
-                let Ok(mut response) = result else {
-                    return result;
-                };
+			let Some(unused_request) = response.extensions_mut().remove::<UnusedRequest>() else {
+				return Ok(response);
+			};
 
-                if response.status() != StatusCode::NOT_FOUND
-                    || !cr.is_subtree_handler()
-                    || !cr.can_handle_request()
-                {
-                    return Ok(response);
-                }
+			request = unused_request.into_request()
+		}
 
-                let Some(unused_request) = response.extensions_mut().remove::<UnusedRequest>()
-                else {
-                    return Ok(response);
-                };
-
-                unused_request.into_request()
-            }
-        };
-    }
-
-    request_handler(request).await
+		request_handler(request).await
+	})
 }
 
-fn request_passer(mut req: Request) -> Pin<Box<dyn Future<Output = Result<Response, BoxedError>>>> {
-    Box::pin(async move {
-        let mut rs = req.extensions_mut().get_mut::<RoutingState>().unwrap();
-        let cr = rs.current_resource.unwrap();
-        let next_path_segment = rs.path_segments.next().unwrap();
+async fn request_passer(mut req: Request) -> Result<Response, BoxedError> {
+	let rs = req.extensions_mut().get_mut::<RoutingState>().unwrap();
+	let cr = rs.current_resource.unwrap();
+	let next_path_segment = rs.path_segments.next().unwrap();
 
-        let some_next_resource = 'some_next_resource: {
-            if let Some(next_resource) = cr.static_resources.as_ref().and_then(|static_resources| {
-                match static_resources
-                    .binary_search_by(|resource| resource.name.cmp(next_path_segment.as_str()))
-                {
-                    Ok(i) => Some(&static_resources[i]),
-                    Err(_) => None,
-                }
-            }) {
-                break 'some_next_resource Some(next_resource);
-            }
+	let some_next_resource = 'some_next_resource: {
+		if let Some(next_resource) = cr.static_resources.as_ref().and_then(|static_resources| {
+			// TODO: Should use a pattern matcher instead of comparing names.
+			match static_resources
+				.binary_search_by(|resource| resource.name.cmp(next_path_segment.as_str()))
+			{
+				Ok(i) => Some(&static_resources[i]),
+				Err(_) => None,
+			}
+		}) {
+			break 'some_next_resource Some(next_resource);
+		}
 
-            None
-        };
+		// TODO: Search for a matching regex resource.
 
-        if let Some(next_resource) = some_next_resource {
-            rs.current_resource.replace(next_resource);
-            let result = request_receiver(req).await;
-            let Ok(mut response) = result else {
-                return result;
-            };
+		// TODO: Return the wildcard resource.
 
-            let Some(unused_request) = response.extensions_mut().remove::<UnusedRequest>() else {
-                return Ok(response);
-            };
+		None
+	};
 
-            req = unused_request.into();
+	if let Some(next_resource) = some_next_resource {
+		rs.current_resource.replace(next_resource);
 
-            rs = req.extensions_mut().get_mut::<RoutingState>().unwrap();
-            rs.current_resource.replace(cr);
-            rs.path_segments.revert_to_segment(next_path_segment);
+		let result = match next_resource.request_receiver.as_ref() {
+			Some(request_receiver) => request_receiver.clone_boxed().call(req).await,
+			None => request_receiver(req).await,
+		};
 
-            response.extensions_mut().insert(UnusedRequest::from(req));
+		let Ok(mut response) = result else {
+			return result;
+		};
 
-            return Ok(response);
-        }
+		let Some(unused_request) = response.extensions_mut().get_mut::<UnusedRequest>() else {
+			return Ok(response);
+		};
 
-        let mut response = Response::default();
-        *response.status_mut() = StatusCode::NOT_FOUND;
-        response.extensions_mut().insert(UnusedRequest::from(req));
+		let req = unused_request.as_mut();
 
-        Ok(response)
-    })
+		let rs = req.extensions_mut().get_mut::<RoutingState>().unwrap();
+		rs.current_resource.replace(cr);
+		rs.path_segments.revert_to_segment(next_path_segment);
+
+		return Ok(response);
+	}
+
+	let mut response = Response::default();
+	*response.status_mut() = StatusCode::NOT_FOUND;
+	response.extensions_mut().insert(UnusedRequest::from(req));
+
+	Ok(response)
 }
 
 async fn request_handler(_req: Request) -> Result<Response, BoxedError> {
