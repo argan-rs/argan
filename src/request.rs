015f4c8bb0409a497886427c4f172cf8222eee21
Shohruh Adham <19423545+shohruhadham@users.noreply.github.com>
2023-06-22 15:30:19 +0500

    feat: add trait FromRequestParts, types Rejection, Error to FromRequest

--- src/request.rs
+++ src/request.rs
@@ -1,3 +1,10 @@
+use hyper::http::request::Parts;
+
+use crate::{
+	response::{IntoResponse, Response},
+	utils::*,
+};
+
 use super::body::*;
 
 // --------------------------------------------------
@@ -6,9 +13,38 @@ pub type Request<B = Incoming> = hyper::Request<B>;
 
 // --------------------------------------------------
 
-pub trait FromRequest<B>
-where
-	B: Body,
-{
-	fn from_request(req: Request<B>) -> Self;
+pub trait FromRequest<B>: Sized {
+	type Rejection: IntoResponse;
+	type Error: Into<BoxedError>;
+
+	fn from_request(req: Request<B>) -> Result<Self, Either<Self::Rejection, Self::Error>>;
+}
+
+impl<B> FromRequest<B> for Request<B> {
+	type Rejection = Response;
+	type Error = BoxedError;
+
+	fn from_request(req: Request<B>) -> Result<Self, Either<Self::Rejection, Self::Error>> {
+		Ok(req)
+	}
+}
+
+// -------------------------
+
+pub trait FromRequestParts: Sized {
+	type Rejection: IntoResponse;
+	type Error: Into<BoxedError>;
+
+	fn from_request_parts(parts: &Parts) -> Result<Self, Either<Self::Rejection, Self::Error>>;
+}
+
+impl<T: FromRequestParts, B> FromRequest<B> for T {
+	type Rejection = T::Rejection;
+	type Error = T::Error;
+
+	fn from_request(req: Request<B>) -> Result<Self, Either<Self::Rejection, Self::Error>> {
+		let (parts, _) = req.into_parts();
+
+		T::from_request_parts(&parts)
+	}
 }
