e3c58b4b6f8a960b3d3d20c4eea8f24c4cd4f4b5
Shohruh Adham <shohruh.adham@gmail.com>
2023-07-03 21:49:08 +0500

    refactor: modify RequestHandler to keep method handlers in a vector

--- src/handler/request_handler.rs
+++ src/handler/request_handler.rs
@@ -9,89 +9,22 @@ use crate::{
 // --------------------------------------------------------------------------------
 
 pub(crate) struct RequestHandler<RqB> {
-	get: Option<HandlerService<RqB>>,
-	put: Option<HandlerService<RqB>>,
-	post: Option<HandlerService<RqB>>,
-	head: Option<HandlerService<RqB>>,
-	patch: Option<HandlerService<RqB>>,
-	trace: Option<HandlerService<RqB>>,
-	delete: Option<HandlerService<RqB>>,
-	options: Option<HandlerService<RqB>>,
-	connect: Option<HandlerService<RqB>>,
-
+	method_handlers: Vec<(Method, HandlerService<RqB>)>,
 	not_allowed_method: Option<HandlerService<RqB>>,
 }
 
 impl<RqB> RequestHandler<RqB> {
 	fn set_handler(&mut self, method: Method, handler: HandlerService<RqB>) {
-		match method {
-			Method::GET => {
-				if self.get.is_some() {
-					panic!("{} handler exists", method)
-				}
+		if self
+			.method_handlers
+			.iter()
+			.find(|(m, _)| m == method)
+			.is_some()
+		{
+			panic!("{} handler already exists", method)
+		}
 
-				self.get = Some(handler);
-			}
-			Method::POST => {
-				if self.post.is_some() {
-					panic!("{} handler exists", method)
-				}
-
-				self.post = Some(handler);
-			}
-			Method::PUT => {
-				if self.put.is_some() {
-					panic!("{} handler exists", method)
-				}
-
-				self.put = Some(handler);
-			}
-			Method::DELETE => {
-				if self.delete.is_some() {
-					panic!("{} handler exists", method)
-				}
-
-				self.delete = Some(handler);
-			}
-			Method::HEAD => {
-				if self.head.is_some() {
-					panic!("{} handler exists", method)
-				}
-
-				self.head = Some(handler);
-			}
-			Method::OPTIONS => {
-				if self.options.is_some() {
-					panic!("{} handler exists", method)
-				}
-
-				self.options = Some(handler);
-			}
-			Method::CONNECT => {
-				if self.connect.is_some() {
-					panic!("{} handler exists", method)
-				}
-
-				self.connect = Some(handler);
-			}
-			Method::PATCH => {
-				if self.patch.is_some() {
-					panic!("{} handler exists", method)
-				}
-
-				self.patch = Some(handler);
-			}
-			Method::TRACE => {
-				if self.trace.is_some() {
-					panic!("{} handler exists", method)
-				}
-
-				self.trace = Some(handler);
-			}
-			_ => {
-				todo!()
-			}
-		};
+		self.method_handlers.push((method, handler));
 	}
 }
 
@@ -105,20 +38,11 @@ where
 
 	fn call(&mut self, request: Request<RqB>) -> Self::Future {
 		let method = request.method().clone();
-		let some_handler = match method {
-			Method::GET => self.get.as_ref().cloned(),
-			Method::POST => self.post.as_ref().cloned(),
-			Method::PUT => self.put.as_ref().cloned(),
-			Method::DELETE => self.delete.as_ref().cloned(),
-			Method::HEAD => self.head.as_ref().cloned(),
-			Method::OPTIONS => self.options.as_ref().cloned(),
-			Method::CONNECT => self.connect.as_ref().cloned(),
-			Method::PATCH => self.patch.as_ref().cloned(),
-			Method::TRACE => self.trace.as_ref().cloned(),
-			_ => {
-				todo!()
-			}
-		};
+		let some_handler = self
+			.method_handlers
+			.iter()
+			.find(|(m, _)| m == method)
+			.map(|(_, h)| h.clone());
 
 		match some_handler {
 			Some(mut handler) => handler.call(request),
