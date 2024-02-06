f4bc704c3af008a026c3c266f26e3921b8f77b1b
Shohruh Adham <shohruh.adham@gmail.com>
2024-02-06 23:34:32 +0500

    feat: add functionality to Resource to modify request extensions

--- src/middleware/internal.rs
+++ src/middleware/internal.rs
@@ -6,47 +6,50 @@ use std::{
 };
 
 use bytes::Bytes;
+use http::Extensions;
 use http_body_util::BodyExt;
 use pin_project::pin_project;
 
 use crate::{
 	body::{Body, HttpBody},
 	common::{BoxedError, BoxedFuture},
-	handler::{Args, BoxedHandler, DummyHandler, Handler},
+	handler::{AdaptiveHandler, Args, BoxedHandler, DummyHandler, Handler},
 	request::Request,
 	resource::ResourceExtensions,
 	response::{IntoResponse, Response},
 };
 
+use super::Layer;
+
 // --------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------
 
-#[derive(Clone)]
-pub(crate) struct RequestBodyAdapter<H>(H);
-
-impl<H, B> Handler<B> for RequestBodyAdapter<H>
-where
-	H: Handler,
-	B: HttpBody<Data = Bytes> + Send + Sync + 'static,
-	B::Error: Into<BoxedError>,
-{
-	type Response = H::Response;
-	type Future = H::Future;
-
-	#[inline]
-	fn handle(&self, request: Request<B>, args: &mut Args) -> Self::Future {
-		let request = request.map(Body::new);
-
-		self.0.handle(request, args)
-	}
-}
-
-impl<H> RequestBodyAdapter<H> {
-	#[inline]
-	pub(crate) fn wrap(handler: H) -> Self {
-		Self(handler)
-	}
-}
+// #[derive(Clone)]
+// pub(crate) struct RequestBodyAdapter<H>(H);
+//
+// impl<H, B> Handler<B> for RequestBodyAdapter<H>
+// where
+// 	H: Handler,
+// 	B: HttpBody<Data = Bytes> + Send + Sync + 'static,
+// 	B::Error: Into<BoxedError>,
+// {
+// 	type Response = H::Response;
+// 	type Future = H::Future;
+//
+// 	#[inline]
+// 	fn handle(&self, request: Request<B>, args: &mut Args) -> Self::Future {
+// 		let request = request.map(Body::new);
+//
+// 		self.0.handle(request, args)
+// 	}
+// }
+//
+// impl<H> RequestBodyAdapter<H> {
+// 	#[inline]
+// 	pub(crate) fn wrap(handler: H) -> Self {
+// 		Self(handler)
+// 	}
+// }
 
 // --------------------------------------------------------------------------------
 
@@ -128,6 +131,94 @@ impl<H> ResponseFutureBoxer<H> {
 	#[inline]
 	pub(crate) fn wrap(handler: H) -> Self {
 		Self(handler)
+	}
+}
+
+// --------------------------------------------------------------------------------
+
+#[derive(Clone)]
+pub(crate) struct RequestExtensionsModifierLayer(BoxedExtensionsModifier);
+
+impl RequestExtensionsModifierLayer {
+	pub(crate) fn new<Func>(modifier: Func) -> Self
+	where
+		Func: Fn(&mut Extensions) + Clone + Send + Sync + 'static,
+	{
+		Self(BoxedExtensionsModifier::new(modifier))
+	}
+}
+
+impl Layer<AdaptiveHandler> for RequestExtensionsModifierLayer {
+	type Handler = RequestExtensionsModifier<AdaptiveHandler>;
+
+	fn wrap(&self, handler: AdaptiveHandler) -> Self::Handler {
+		RequestExtensionsModifier::new(handler, self.0.clone())
+	}
+}
+
+// ----------
+
+#[derive(Clone)]
+pub(crate) struct RequestExtensionsModifier<H> {
+	inner_handler: H,
+	boxed_modifier: BoxedExtensionsModifier,
+}
+
+impl<H> RequestExtensionsModifier<H> {
+	fn new(handler: H, boxed_modifier: BoxedExtensionsModifier) -> Self {
+		Self {
+			inner_handler: handler,
+			boxed_modifier,
+		}
+	}
+}
+
+impl<H, B> Handler<B> for RequestExtensionsModifier<H>
+where
+	H: Handler<B>,
+{
+	type Response = H::Response;
+	type Future = H::Future;
+
+	#[inline(always)]
+	fn handle(&self, mut request: Request<B>, args: &mut Args<'_, ()>) -> Self::Future {
+		self.boxed_modifier.0(request.extensions_mut());
+
+		self.inner_handler.handle(request, args)
+	}
+}
+
+// -------------------------
+
+trait ExtensionsModifier: Fn(&mut Extensions) {
+	fn boxed_clone(&self) -> BoxedExtensionsModifier;
+}
+
+impl<Func> ExtensionsModifier for Func
+where
+	Func: Fn(&mut Extensions) + Clone + Send + Sync + 'static,
+{
+	fn boxed_clone(&self) -> BoxedExtensionsModifier {
+		BoxedExtensionsModifier::new(self.clone())
+	}
+}
+
+// -------------------------
+
+struct BoxedExtensionsModifier(Box<dyn ExtensionsModifier + Send + Sync + 'static>);
+
+impl BoxedExtensionsModifier {
+	pub(crate) fn new<Func>(modifier: Func) -> Self
+	where
+		Func: Fn(&mut Extensions) + Clone + Send + Sync + 'static,
+	{
+		Self(Box::new(modifier))
+	}
+}
+
+impl Clone for BoxedExtensionsModifier {
+	fn clone(&self) -> Self {
+		self.0.boxed_clone()
 	}
 }
 
